897d98e7f840a082bef9ae022d5da51ba644c883
Gedihared6409 <gediali019@gmail.com>
2020-05-06 16:40:20 +0300

    Regestering the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,8 @@ mail = Mail(app)
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
 
+def create_app(config_name):
+    app.config.from_object(config_options[config_name])
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
