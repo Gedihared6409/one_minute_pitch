3eb5c7b0277af88991c14e3eed1b867e63fbcd56
Gedihared6409 <gediali019@gmail.com>
2020-05-06 16:54:04 +0300

    added a pitcher form and comment form

--- app/main/errors.py
+++ app/main/errors.py
@@ -4,8 +4,6 @@ from . import main
 # Error handler decorator
 @main.app_errorhandler(404)
 def four_Ow_four(error):
-    '''
-    Function to render the 404 error page
-    '''
+
     title = '404 page'
     return render_template('fourOwfour.html', title=title),404
---
+++ app/main/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,SubmitField,TextAreaField,RadioField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+
+
+
+class PitchForm(FlaskForm):
+	title = StringField('Title', validators=[Required()])
+	description = TextAreaField("What do you want to pitch about ?",validators=[Required()])
+	category = RadioField('Label', choices=[ ('Pickuplinespitch','pickuplinespitch'), ('interviewpitch','interviewpitch'),('bussinessplan','bussinessplan'),('promotionpitch','promotionpitch')],validators=[Required()])
+	submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
